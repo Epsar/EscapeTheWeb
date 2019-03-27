bb35661525b3580d085d8ccf960416b9c1cc566a
Epsar <45588824+Epsar@users.noreply.github.com>
2019-03-27 18:37:00 +0100

    pas fini

--- EscapeTheLab/includes/opensession.php
+++ EscapeTheLab/includes/opensession.php
@@ -1,10 +1,9 @@
 <?php 
-$addrequest= $bdd-> prepare("INSERT INTO SESSION (jeu, nom) VALUES (:jeu, :nom)");
-$fjeu = $_POST['gamename'];
-$fnom = $_POST['login2'];
-$addrequest->bindvalue('jeu', $fjeu, PDO::PARAM_STR);
-$addrequest->bindvalue('nom', $fnom, PDO::PARAM_STR);
-$addrequest->execute();
-$_SESSION['_id'] = $bdd->lastInsertId(); 
-
+    $addrequest= $bdd-> prepare("INSERT INTO SESSION (jeu, nom) VALUES (:jeu, :nom)");
+    $fjeu = $_POST['gamename'];
+    $fnom = $_POST['login2'];
+    $addrequest->bindvalue('jeu', $fjeu, PDO::PARAM_STR);
+    $addrequest->bindvalue('nom', $fnom, PDO::PARAM_STR);
+    $addrequest->execute();
+    $_SESSION['_id'] = $bdd->lastInsertId(); 
 ?>
--- EscapeTheLab/pageadmin.php
+++ EscapeTheLab/pageadmin.php
@@ -71,17 +71,19 @@
                 ?>
             </div>
         </div>
+        <br /><br /><br />
+
 
         <div class="container">
             <div class="row">
                 <div class="col-md-5">
                     <div class="panel panel-primary">
                         <div class="panel-heading">
-                            <span class="glyphicon glyphicon-comment"></span> Chat
-                            <div class="btn-group pull-right">
+                            <span class="glyphicon glyphicon-envelope"></span> Chat
+                            <!-- <div class="btn-group pull-right">
                                 <button type="button" class="btn btn-default btn-xs dropdown-toggle"
                                     data-toggle="dropdown">
-                                    <span class="glyphicon glyphicon-chevron-down"></span>
+                                    <span class="glyphicon glyphicon-arrow-down"></span>
                                 </button>
                                 <ul class="dropdown-menu slidedown">
                                     <li><a href="#"><span class="glyphicon glyphicon-refresh"></span>Refresh</a></li>
@@ -91,91 +93,36 @@
                                     <li class="divider"></li>
                                     <li><a href="#"><span class="glyphicon glyphicon-off"></span>Sign Out</a></li>
                                 </ul>
-                            </div>
+                            </div> -->
                         </div>
+
+
                         <div class="panel-body">
                             <ul class="chat">
-                                <li class="left clearfix">
-                                    <span class="chat-img pull-left">
-                                        <img src="http://placehold.it/50/55C1E7/fff&text=JS" alt="User Avatar"
-                                            class="img-circle" />
-                                    </span>
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <strong class="primary-font">Jack Sparrow</strong> <small
-                                                class="pull-right text-muted">
-                                                <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
-                                            ornare dolor, quis ullamcorper ligula sodales.
-                                        </p>
-                                    </div>
-                                </li>
-                                <li class="right clearfix"><span class="chat-img pull-right">
-                                        <img src="http://placehold.it/50/FA6F57/fff&text=BP" alt="User Avatar"
-                                            class="img-circle" />
-                                    </span>
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>13
-                                                mins ago</small>
-                                            <strong class="pull-right primary-font">Bhaumik Patel</strong>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
-                                            ornare dolor, quis ullamcorper ligula sodales.
-                                        </p>
-                                    </div>
-                                </li>
-                                <li class="left clearfix"><span class="chat-img pull-left">
-                                        <img src="http://placehold.it/50/55C1E7/fff&text=JS" alt="User Avatar"
-                                            class="img-circle" />
-                                    </span>
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <strong class="primary-font">Jack Sparrow</strong> <small
-                                                class="pull-right text-muted">
-                                                <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
-                                            ornare dolor, quis ullamcorper ligula sodales.
-                                        </p>
-                                    </div>
-                                </li>
-                                <li class="right clearfix"><span class="chat-img pull-right">
-                                        <img src="http://placehold.it/50/FA6F57/fff&text=BP" alt="User Avatar"
-                                            class="img-circle" />
-                                    </span>
-                                    <div class="chat-body clearfix">
-                                        <div class="header">
-                                            <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>15
-                                                mins ago</small>
-                                            <strong class="pull-right primary-font">Bhaumik Patel</strong>
-                                        </div>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
-                                            ornare dolor, quis ullamcorper ligula sodales.
-                                        </p>
-                                    </div>
-                                </li>
+                                <?php
+                                    include('includes/chat.php');
+
+                                ?>
+
                             </ul>
                         </div>
+
+
+
                         <div class="panel-footer">
                             <div class="input-group">
-                                <input id="btn-input" type="text" class="form-control input-sm"
-                                    placeholder="Type your message here..." />
+                                <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
                                 <span class="input-group-btn">
                                     <button class="btn btn-warning btn-sm" id="btn-chat">
-                                        Send</button>
+                                        Send
+                                    </button>
                                 </span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div> 
+        </div>
 
 
         <script src="../includes/chat.js"></script>
@@ -186,3 +133,39 @@
 </body>
 
 </html>
+
+
+
+
+
+
+
+<!-- <li class="left clearfix"><span class="chat-img pull-left">
+        <img src="http://placehold.it/50/55C1E7/fff&text=JS" alt="User Avatar" class="img-circle" />
+    </span>
+    <div class="chat-body clearfix">
+        <div class="header">
+            <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
+        </div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+            ornare dolor, quis ullamcorper ligula sodales.
+        </p>
+    </div>
+</li>
+<li class="right clearfix"><span class="chat-img pull-right">
+        <img src="http://placehold.it/50/FA6F57/fff&text=BP" alt="User Avatar" class="img-circle" />
+    </span>
+    <div class="chat-body clearfix">
+        <div class="header">
+            <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>15
+                mins ago</small>
+            <strong class="pull-right primary-font">Bhaumik Patel</strong>
+        </div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+            ornare dolor, quis ullamcorper ligula sodales.
+        </p>
+    </div>
+</li> -->
